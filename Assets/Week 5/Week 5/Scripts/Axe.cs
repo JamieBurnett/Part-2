a23e4c634b9ef8f91f42697acfef01b5905b65ea
JamieBurnett <jamieburny19@outlook.com>
2024-02-08 18:39:36 -0500

    Weapon Prefab (4b)
    the axe will fly accross the screen, deal damage and delete itself on a hit, and also destroys itself after 6 seconds

---
+++ Assets/Week 5/Week 5/Scripts/Axe.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    Vector2 velocity;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,6);
+        rb = GetComponent<Rigidbody2D>();
+        velocity = new Vector2(-5, 0);
+        rb.velocity = velocity;
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", 1);
+        Destroy(gameObject);
+    }
+}
