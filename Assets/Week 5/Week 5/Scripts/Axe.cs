e666f14c0d438e458b8049f850a890896511f48e
Jamie.Burnett <jamieburny19@gmail.com>
2024-02-08 15:55:56 -0500

    This contains task 1. Had some issues w/ git you helped me resolve and you told me to just make a commit for this that included the code, hence the weird timestamp

---
+++ Assets/Week 5/Week 5/Scripts/Axe.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    public float lifespan;
+    private float direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
