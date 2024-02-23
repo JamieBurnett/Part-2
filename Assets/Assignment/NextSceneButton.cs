7f3219fa3cbae5703cf7ca193b67463d47bf7986
JamieBurnett <jamieburny19@outlook.com>
2024-02-23 16:08:15 -0500

    main menu added

---
+++ Assets/Assignment/NextSceneButton.cs
@@ -0,0 +1,35 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneButton : MonoBehaviour
+{
+    private int sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void NextScene()
+    {
+        if(sceneIndex +1 < SceneManager.sceneCountInBuildSettings)
+        {
+            sceneIndex += 1;
+        }
+        else
+        {
+            sceneIndex = 0;
+        }
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
